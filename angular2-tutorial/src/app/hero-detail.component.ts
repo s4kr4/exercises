d3107c10d271005ff7ff3dba350daaf9decfdff0
s4kr4 <s4kr4m4@gmail.com>
2017-03-31 07:31:43 +0900

    Fix hero property

--- angular2-tutorial/src/app/hero-detail.component.ts
+++ angular2-tutorial/src/app/hero-detail.component.ts
@@ -12,6 +12,7 @@ import { Hero } from './hero';
   styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
   constructor(
     private heroService: HeroService,
